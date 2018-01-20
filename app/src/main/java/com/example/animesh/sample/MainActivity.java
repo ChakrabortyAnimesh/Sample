c1a5c9c27cc3ddb2465ba3537948564196a5e76a
Animesh <animesh.b.tech12345@gmail.com>
2018-01-20 14:46:32 -0800

    adding second toast

--- app/src/main/java/com/example/animesh/sample/MainActivity.java
+++ app/src/main/java/com/example/animesh/sample/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hello world", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hi this is my second toast ", Toast.LENGTH_SHORT).show();
     }
 }
